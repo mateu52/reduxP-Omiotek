925dc2ea7a4c72d474987a9dedc1fac41e25565a
mateu52 <wuoelte@gmail.com>
2022-10-03 21:02:41 +0200

    .env add

--- src/contact/contact.js
+++ src/contact/contact.js
@@ -2,13 +2,16 @@ import React from "react";
 import { useForm } from "react-hook-form";
 function Contact(){
     const { register, handleSubmit } = useForm();
-    const onSubmit = data => console.log(data);
+    const onSubmit = data => {
+        //require("dotenv-webpack");
+       
 //////////////////////////////////////////
     const headers = new Headers()
         headers.append("Content-Type", "application/json")
 
     const body = {
-        "test": "event"
+        "test": "event12",
+        "name": data.firstname
     }
 
     const options = {
@@ -17,10 +20,11 @@ function Contact(){
         mode: "cors",
         body: JSON.stringify(body),
     }
+
 //////////////////////////////////////////////
-fetch("https://eoq2uuazn32z1ot.m.pipedream.net", options)
-
-
+fetch(process.env.REACT_APP_CONTACT_FORM_URL, options)
+}
+console.log(process.env.REACT_APP_CONTACT_FORM_URL)
     return (
         <form onSubmit={handleSubmit(onSubmit)}>
             <input {...register('firstname')} placeholder="first name" />
